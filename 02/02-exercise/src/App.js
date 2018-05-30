47fa17cfe18e624268362fc484a2cf33b10d4274
Daniyar Jakupov <ddr199305@gmail.com>
2018-05-30 23:27:07 +0200

    Solution to RadioBtns (React.cloneElement)

--- 02/02-exercise/src/App.js
+++ 02/02-exercise/src/App.js
@@ -1,30 +1,43 @@
-import './index.css'
-import React, { Component } from 'react'
-import FaPlay from 'react-icons/lib/fa/play'
-import FaPause from 'react-icons/lib/fa/pause'
-import FaForward from 'react-icons/lib/fa/forward'
-import FaBackward from 'react-icons/lib/fa/backward'
+import "./index.css";
+import React, { Component } from "react";
+import FaPlay from "react-icons/lib/fa/play";
+import FaPause from "react-icons/lib/fa/pause";
+import FaForward from "react-icons/lib/fa/forward";
+import FaBackward from "react-icons/lib/fa/backward";
 
 class RadioGroup extends Component {
+  state = {
+    activeBtn: this.props.defaultValue
+  };
+
   render() {
+    const children = React.Children.map(this.props.children, child => {
+      return React.cloneElement(child, {
+        isActive: this.state.activeBtn === child.props.value,
+        btnValue: child.props.value,
+        onBtnClick: () => this.setState({ activeBtn: child.props.value })
+      });
+    });
+
     return (
       <fieldset className="radio-group">
         <legend>{this.props.legend}</legend>
-        {this.props.children}
+        {children}
       </fieldset>
-    )
+    );
   }
 }
 
 class RadioButton extends Component {
   render() {
-    const isActive = false // <-- should come from somewhere
-    const className = 'radio-button ' + (isActive ? 'active' : '')
+    const { isActive, onBtnClick } = this.props;
+    const className = "radio-button " + (isActive ? "active" : "");
+
     return (
-      <button className={className}>
+      <button className={className} onClick={onBtnClick}>
         {this.props.children}
       </button>
-    )
+    );
   }
 }
 
@@ -32,15 +45,23 @@ class App extends Component {
   render() {
     return (
       <div>
-        <RadioGroup legend="Radio Group">
-          <RadioButton value="back"><FaBackward/></RadioButton>
-          <RadioButton value="play"><FaPlay/></RadioButton>
-          <RadioButton value="pause"><FaPause/></RadioButton>
-          <RadioButton value="forward"><FaForward/></RadioButton>
+        <RadioGroup legend="Radio Group" defaultValue="pause">
+          <RadioButton value="back">
+            <FaBackward />
+          </RadioButton>
+          <RadioButton value="play">
+            <FaPlay />
+          </RadioButton>
+          <RadioButton value="pause">
+            <FaPause />
+          </RadioButton>
+          <RadioButton value="forward">
+            <FaForward />
+          </RadioButton>
         </RadioGroup>
       </div>
-    )
+    );
   }
 }
 
-export default App
+export default App;
