d666e3ece19954a277e18954b58b322e7da32db2
Daniyar Jakupov <ddr199305@gmail.com>
2018-06-28 14:07:10 +0200

    Use old Context API

--- 03/03-exercise/src/App.js
+++ 03/03-exercise/src/App.js
@@ -43,123 +43,219 @@ Other notes about the `<audio/>` tag:
 Good luck!
 */
 
-import './index.css'
-import React from 'react'
-import * as PropTypes from 'prop-types'
-import podcast from './podcast.mp3'
-import mario from './mariobros.mp3'
-import FaPause from 'react-icons/lib/fa/pause'
-import FaPlay from 'react-icons/lib/fa/play'
-import FaRepeat from 'react-icons/lib/fa/repeat'
-import FaRotateLeft from 'react-icons/lib/fa/rotate-left'
+import "./index.css";
+import React from "react";
+import * as PropTypes from "prop-types";
+import podcast from "./podcast.mp3";
+import mario from "./mariobros.mp3";
+import FaPause from "react-icons/lib/fa/pause";
+import FaPlay from "react-icons/lib/fa/play";
+import FaRepeat from "react-icons/lib/fa/repeat";
+import FaRotateLeft from "react-icons/lib/fa/rotate-left";
 
 class AudioPlayer extends React.Component {
+  static propTypes = {
+    source: PropTypes.string.isRequired
+  };
+
+  static childContextTypes = {
+    audio: PropTypes.object.isRequired
+  };
+
+  state = {
+    isPlaying: false,
+    duration: null,
+    loaded: false,
+    currentTime: 0
+  };
+
+  getChildContext() {
+    return {
+      audio: {
+        ...this.state,
+        play: () => {
+          this.audio.play();
+          this.setState({ isPlaying: true });
+        },
+        pause: () => {
+          this.audio.pause();
+          this.setState({ isPlaying: false });
+        },
+        jump: time => {
+          this.audio.currentTime = this.audio.currentTime + time;
+        },
+        setTime: time => (this.audio.currentTime = time)
+      }
+    };
+  }
+
+  onAudioLoad = () => {
+    this.setState({ loaded: true, duration: this.audio.duration });
+  };
+
+  onTimeUpdate = () => {
+    this.setState({ currentTime: this.audio.currentTime });
+  };
+
+  onAudioEnd = () => {
+    this.setState({ isPlaying: false });
+  };
+
   render() {
     return (
       <div className="audio-player">
         <audio
-          src={null}
-          onTimeUpdate={null}
-          onLoadedData={null}
-          onEnded={null}
-          ref={n => this.audio = n}
+          src={this.props.source}
+          onTimeUpdate={this.onTimeUpdate}
+          onLoadedData={this.onAudioLoad}
+          onEnded={this.onAudioEnd}
+          ref={n => (this.audio = n)}
         />
         {this.props.children}
       </div>
-    )
+    );
   }
 }
 
 class Play extends React.Component {
+  static contextTypes = {
+    audio: PropTypes.object.isRequired
+  };
+
   render() {
     return (
       <button
         className="icon-button"
-        onClick={null}
-        disabled={null}
+        onClick={this.context.audio.play}
+        disabled={this.context.audio.isPlaying}
         title="play"
-      ><FaPlay/></button>
-    )
+      >
+        <FaPlay />
+      </button>
+    );
   }
 }
 
 class Pause extends React.Component {
+  static contextTypes = {
+    audio: PropTypes.object.isRequired
+  };
+
   render() {
     return (
       <button
         className="icon-button"
-        onClick={null}
-        disabled={null}
+        onClick={this.context.audio.pause}
+        disabled={!this.context.audio.isPlaying}
         title="pause"
-      ><FaPause/></button>
-    )
+      >
+        <FaPause />
+      </button>
+    );
   }
 }
 
 class PlayPause extends React.Component {
+  static contextTypes = {
+    audio: PropTypes.object.isRequired
+  };
+
   render() {
-    return null
+    const { isPlaying } = this.context.audio;
+
+    return isPlaying ? <Pause /> : <Play />;
   }
 }
 
 class JumpForward extends React.Component {
+  static contextTypes = {
+    audio: PropTypes.object.isRequired
+  };
+
   render() {
+    const { isPlaying } = this.context.audio;
     return (
       <button
         className="icon-button"
-        onClick={null}
-        disabled={null}
+        onClick={() => this.context.audio.jump(10)}
+        disabled={!isPlaying}
         title="Forward 10 Seconds"
-      ><FaRepeat/></button>
-    )
+      >
+        <FaRepeat />
+      </button>
+    );
   }
 }
 
 class JumpBack extends React.Component {
+  static contextTypes = {
+    audio: PropTypes.object.isRequired
+  };
+
   render() {
+    const { isPlaying } = this.context.audio;
     return (
       <button
         className="icon-button"
-        onClick={null}
-        disabled={null}
+        onClick={() => this.context.audio.jump(-10)}
+        disabled={!isPlaying}
         title="Back 10 Seconds"
-      ><FaRotateLeft/></button>
-    )
+      >
+        <FaRotateLeft />
+      </button>
+    );
   }
 }
 
 class Progress extends React.Component {
+  static contextTypes = {
+    audio: PropTypes.object.isRequired
+  };
+
+  handleTimeJump = event => {
+    const rect = this.node.getBoundingClientRect();
+    const relativeLeft = event.clientX - rect.left;
+    const multiplier = relativeLeft / rect.width;
+    const time = multiplier * this.context.audio.duration;
+
+    this.context.audio.setTime(time);
+  };
+
   render() {
+    const { currentTime, duration, loaded } = this.context.audio;
+    const width = loaded ? currentTime / duration : 0;
     return (
       <div
         className="progress"
-        onClick={null}
+        ref={node => (this.node = node)}
+        onClick={this.handleTimeJump}
       >
         <div
           className="progress-bar"
           style={{
-            width: '23%'
+            width: `${width * 100}%`
           }}
         />
       </div>
-    )
+    );
   }
 }
 
 const Exercise = () => (
   <div className="exercise">
     <AudioPlayer source={mario}>
-      <Play/> <Pause/>{' '}
-      <span className="player-text">Mario Bros. Remix</span>
-      <Progress/>
+      <Play /> <Pause /> <span className="player-text">Mario Bros. Remix</span>
+      <Progress />
     </AudioPlayer>
 
     <AudioPlayer source={podcast}>
-      <PlayPause/> <JumpBack/> <JumpForward/> {' '}
-      <span className="player-text">React30 Episode 010: React Virtualized</span>
-      <Progress/>
+      <PlayPause /> <JumpBack /> <JumpForward />{" "}
+      <span className="player-text">
+        React30 Episode 010: React Virtualized
+      </span>
+      <Progress />
     </AudioPlayer>
   </div>
-)
+);
 
-export default Exercise
+export default Exercise;
