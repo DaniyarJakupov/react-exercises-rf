31949563c25709cecb6283d69ccb25d20a661274
Daniyar Jakupov <ddr199305@gmail.com>
2018-07-02 10:52:26 +0200

    Add solution to Controlled & Uncotrolled components

--- 08/08-exercise/src/App.js
+++ 08/08-exercise/src/App.js
@@ -1,54 +1,81 @@
-import './index.css'
-import React, { PropTypes } from 'react'
+import "./index.css";
+import React, { PropTypes } from "react";
 
+// Can be either controlled or uncontroled component
 class Select extends React.Component {
   static propTypes = {
     onChange: PropTypes.func,
     value: PropTypes.any,
     defaultValue: PropTypes.any
-  }
+  };
+
+  state = {
+    value: this.props.defaultValue,
+    isOpen: false
+  };
+
+  isControlled = () => {
+    return this.props.value != null;
+  };
+
+  handleToggle = () => {
+    this.setState(prevState => ({ isOpen: !prevState.isOpen }));
+  };
 
   render() {
-    const isOpen = false
+    const { isOpen } = this.state;
+    let label;
+    const { value } = this.isControlled() ? this.props : this.state;
+
+    const children = React.Children.map(this.props.children, child => {
+      if (child.props.value === value) {
+        label = child.props.children;
+      }
+      return React.cloneElement(child, {
+        onSelect: () => {
+          this.props.onChange(child.props.value);
+          if (!this.isControlled()) {
+            this.setState({ value: child.props.value });
+          }
+        }
+      });
+    });
+
     return (
-      <div className="select">
-        <div className="label">label <span className="arrow">▾</span></div>
-        {isOpen && (
-          <div className="options">
-            {children}
-          </div>
-        )}
+      <div className="select" onClick={this.handleToggle}>
+        <div className="label">
+          {label} <span className="arrow">▾</span>
+        </div>
+        {isOpen && <div className="options">{children}</div>}
       </div>
-    )
+    );
   }
 }
-
 
 class Option extends React.Component {
   render() {
     return (
-      <div className="option">
+      <div className="option" onClick={this.props.onSelect}>
         {this.props.children}
       </div>
-    )
+    );
   }
 }
 
 class App extends React.Component {
   state = {
-    selectValue: 'dosa'
-  }
+    selectValue: "dosa"
+  };
 
   setToMintChutney = () => {
     this.setState({
-      selectValue: 'mint-chutney'
-    })
-  }
+      selectValue: "mint-chutney"
+    });
+  };
 
   render() {
     return (
       <div className="app">
-
         <div className="block">
           <h2>Uncontrolled</h2>
           <Select defaultValue="tikka-masala">
@@ -62,14 +89,12 @@ class App extends React.Component {
         <div className="block">
           <h2>Controlled</h2>
           <p>
-            <button onClick={this.setToMintChutney}>
-              Set to Mint Chutney
-            </button>
+            <button onClick={this.setToMintChutney}>Set to Mint Chutney</button>
           </p>
           <Select
             value={this.state.selectValue}
-            onChange={(selectValue) => {
-              this.setState({ selectValue })
+            onChange={selectValue => {
+              this.setState({ selectValue });
             }}
           >
             <Option value="tikka-masala">Tikka Masala</Option>
@@ -79,8 +104,8 @@ class App extends React.Component {
           </Select>
         </div>
       </div>
-    )
+    );
   }
 }
 
-export default App
+export default App;
